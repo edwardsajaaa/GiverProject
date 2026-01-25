df022e2f14fc5e83cecbeed92fa0df24751e620a
edwardsaja <edwardbene07@gmail.com>
2026-01-25 23:01:28 +0800

    Initial project commit: 3D web scene with React Three Fiber

---
+++ my-3d-web/src/App.jsx
@@ -0,0 +1,26 @@
+
+
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls } from '@react-three/drei';
+
+function Box() {
+  return (
+    <mesh>
+      <boxGeometry args={[1, 1, 1]} />
+      <meshStandardMaterial color="#4f8cff" />
+    </mesh>
+  );
+}
+
+export default function App() {
+  return (
+    <div style={{ width: '100vw', height: '100vh', margin: 0, padding: 0, overflow: 'hidden' }}>
+      <Canvas camera={{ position: [3, 3, 3], fov: 60 }} style={{ background: '#181c20' }}>
+        <ambientLight intensity={0.7} />
+        <directionalLight position={[5, 10, 7]} intensity={1} />
+        <Box />
+        <OrbitControls enablePan={false} />
+      </Canvas>
+    </div>
+  );
+}
